48f8ffaf8f06b14cb066563765ac43410086eb8e
ezl <ericzliu@gmail.com>
2019-06-22 17:39:25 -0500

    vue project stub and basic page shape

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Generate 1 on 1 pairings</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/ezl/"
+        target="_blank"
+      >
+        <span class="mr-2">Github</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <Main/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    Main
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex mb-4>
+        <v-flex xs12 sm9 md6>
+          <v-textarea
+            box
+            name="participants"
+            v-model="participants"
+            label="Enter participant names here (1 per line)"
+          ></v-textarea>
+          <v-btn depressed small color="primary" @click="generatePairings">Generate Pairings</v-btn>
+        </v-flex>
+
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify
+        </h1>
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+        </p>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      participants: '',
+
+    }),
+    methods: {
+      generatePairings() {
+        console.log(this.participants)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
